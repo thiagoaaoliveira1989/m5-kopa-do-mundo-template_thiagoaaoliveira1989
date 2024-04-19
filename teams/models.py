23990fa21f91844f43e5bf278e9ca32760a1efd4
thiagoaaoliveira1989 <thiagoaaoliveira1989@gmail.com>
2024-04-18 23:36:23 -0300

    feat: routes post and get created

--- teams/models.py
+++ teams/models.py
@@ -6,7 +6,7 @@ class Team(models.Model):
     titles = models.IntegerField(default=0)
     top_scorer = models.CharField(max_length=50)
     fifa_code = models.CharField(max_length=3)
-    fifa_cup = models.DateField(null=True, blank=True)
+    first_cup = models.DateField(null=True)
 
     def __repr__(self):
         return f"<[{self.pk}] {self.name} - {self.fifa_code}>"
--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from teams.models import Team
+from django.forms.models import model_to_dict
+from utils import data_processing
+from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError
 
-# Create your views here.
+
+class TeamView(APIView):
+    def post(self, request):
+        try:
+            team_data = request.data
+            data_processing(team_data)
+            team = Team.objects.create(
+                name=team_data["name"],
+                titles=team_data["titles"],
+                top_scorer=team_data["top_scorer"],
+                fifa_code=team_data["fifa_code"],
+                first_cup=team_data["first_cup"],
+            )
+
+            return Response(model_to_dict(team), 201)
+        except (NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError) as err:
+            return Response({"Error ": str(err)}, status=400)
+
+    def get(self, request):
+        teams = Team.objects.all()
+
+        teams_dic = []
+
+        for team in teams:
+            t = model_to_dict(team)
+            teams_dic.append(t)
+
+        return Response(teams_dic)
